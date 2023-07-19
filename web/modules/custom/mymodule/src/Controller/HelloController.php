7b3b0f4194d0020fb9bf780d00ff84c8102c9dd1
uttamkarmakar <uttam.karmakar@innoraft.com>
2023-07-19 15:33:52 +0530

    FT2023-359: Updated the 359 ticket tasks using services and DI.

--- web/modules/custom/mymodule/src/Controller/HelloController.php
+++ web/modules/custom/mymodule/src/Controller/HelloController.php
@@ -3,27 +3,60 @@
 namespace Drupal\mymodule\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * This controller helps to display a current user name using it's method
- * 
+ * This controller helps to display a current user name using its method.
+ *
  * @package Drupal\mymodule\Controller
- * 
- * @author Uttam Karmakar <uttam.karmakar@innoraft.com>
  */
 class HelloController extends ControllerBase
 {
   /**
-   * @method helloMethod
-   *  By the help of this method the name of current user will be displayed on
-   *  the page
+   * The current user service.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Constructor for HelloController.
+   *
+   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   *   The current user service.
+   */
+  public function __construct(AccountInterface $current_user) {
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * Creates a new instance of the HelloController.
+   *
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   The dependency injection container.
+   *
+   * @return \Drupal\mymodule\Controller\HelloController
+   *   The HelloController instance.
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('current_user')
+    );
+  }
+
+
+  /**
+   * Display the name of the current user on the page.
+   *
+   * @return array
+   *   A renderable array.
    */
   public function helloMethod()
   {
-    $user = $this->currentUser();
+    $user = $this->currentUser;
     $getUserName = $user->getDisplayName();
 
-    // Create a cache metadata object.
     $cacheMetadata = new \Drupal\Core\Cache\CacheableMetadata();
     $cacheMetadata->setCacheTags(['user:' . $user->id()]);
 
@@ -35,3 +68,4 @@ class HelloController extends ControllerBase
     ];
   }
 }
+
